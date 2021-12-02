21f95d3291272e4e2cef7ff7d91425266439a88d
ARJUN B <arjunb0320@gmail.com>
2021-12-02 08:31:38 +0530

    initial commit

---
+++ javaScript/script.js
@@ -0,0 +1,8 @@
+// function validate(){
+//     if(document.t.n1.value==" "){
+//         alert("enter the name")
+//     }
+//     if(document.getElementById('email').value==" "){
+//         alert("enter email")
+//     }
+// }
